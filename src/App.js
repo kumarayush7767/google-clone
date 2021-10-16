4ad88614d01f0a4dbe0ef42c2b9e1ee2bc174d0e
Ayush Kumar <kumarayush7767@gmail.com>
2021-10-16 20:02:31 +0530

    adding search page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./components/SearchPage";
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
           <Route path="/search">
-            <h1>This is the search page</h1>
+            <SearchPage/>
           </Route>
           <Route path="/">
             <Home />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,35 @@
-import React from 'react'
-import './Home.css';
-import {Link} from 'react-router-dom';
-import AppsIcon from '@material-ui/icons/Apps';
-import { Avatar } from '@material-ui/core';
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
+import Search from "../components/Search.js";
 const Home = () => {
-    return (
-        <div className="home">
-            <h1>Hey this is ayush</h1>
-            <div className="home__header">
-              <div className="home__headerLeft">
-                 <Link to='/about'>
-                     About
-                 </Link>
-                 <Link to='/store'>
-                     Store
-                 </Link>
-              </div>
-              <div className="home__headerRight">
-                 <Link to='/gmail'>
-                     Gmail
-                 </Link>
-                 <Link to='/images'>
-                     Images
-                 </Link>
-                 <AppsIcon/>
-                 <Avatar/>
-              </div>
-            </div>
-            <div className="home__body">
-               
-            </div>
+  return (
+    <div className="home">
+      <div className="home__header">
+        <div className="home__headerLeft">
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
         </div>
-    )
-}
+        <div className="home__headerRight">
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/images">Images</Link>
+          <AppsIcon />
+          <Avatar />
+        </div>
+      </div>
+      <div className="home__body">
+        <img
+          src="https://www.freepnglogos.com/uploads/google-logo-png/file-google-logo-svg-wikimedia-commons-23.png"
+          alt=""
+        />
+        <div className="home__inputContainer">
+          <Search hideButtons />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
